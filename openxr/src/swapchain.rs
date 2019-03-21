ed3cdde91ffc402670e90a5feb46582479b395a8
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-03-20 19:13:52 -0700

    Swapchain image handling

--- openxr/src/swapchain.rs
+++ openxr/src/swapchain.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{ptr, marker::PhantomData};
 
 use crate::*;
 
@@ -35,8 +35,51 @@ impl<G: Graphics> Swapchain<G> {
         self.session.instance()
     }
 
-    pub fn enumerate_swapchain_images(&self) -> Result<Vec<G::SwapchainImage>> {
+    pub fn enumerate_images(&self) -> Result<Vec<G::SwapchainImage>> {
         G::enumerate_swapchain_images(self)
+    }
+
+    /// Determine the index of the next image to render to in the swapchain image array
+    pub fn acquire_image(&self) -> Result<u32> {
+        let info = sys::SwapchainImageAcquireInfo {
+            ty: sys::SwapchainImageAcquireInfo::TYPE,
+            next: ptr::null_mut(),
+        };
+        let mut out = 0;
+        unsafe {
+            cvt((self.fp().acquire_swapchain_image)(self.as_raw(), &info, &mut out))?;
+        }
+        Ok(out)
+    }
+
+    /// Wait for the compositor to finish reading from the oldest unwaited acquired image
+    ///
+    /// # Safety
+    ///
+    /// Once a swapchain image has been successfully waited on, it must be released before waiting
+    /// on the next acquired swapchain image.
+    pub unsafe fn wait_image(&self, timeout: Duration) -> Result<()> {
+        let info = sys::SwapchainImageWaitInfo {
+            ty: sys::SwapchainImageWaitInfo::TYPE,
+            next: ptr::null_mut(),
+            timeout,
+        };
+        cvt((self.fp().wait_swapchain_image)(self.as_raw(), &info))?;
+        Ok(())
+    }
+
+    /// Release the oldest acquired image
+    ///
+    /// # Safety
+    ///
+    /// The swapchain image must have been successfully waited on before it is released.
+    pub unsafe fn release_image(&self) -> Result<()> {
+        let info = sys::SwapchainImageReleaseInfo {
+            ty: sys::SwapchainImageReleaseInfo::TYPE,
+            next: ptr::null_mut(),
+        };
+        cvt((self.fp().release_swapchain_image)(self.as_raw(), &info))?;
+        Ok(())
     }
 
     // Private helper
