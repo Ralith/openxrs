c72fa556e6587bc7336e3a823469e6a6f0606622
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-03-20 20:11:04 -0700

    Hide details of Graphics

--- openxr/src/graphics/mod.rs
+++ openxr/src/graphics/mod.rs
@@ -1,24 +1,36 @@
 use crate::*;
 
 /// Static dispatch for OpenXR graphics bindings
+///
+/// The types and functions defined by this trait are an implementation detail, and should not be
+/// referenced externally.
 pub trait Graphics: Sized {
+    #[doc(hidden)]
     type Requirements;
+    #[doc(hidden)]
     type SessionCreateInfo;
     /// Swapchain image formats
+    #[doc(hidden)]
     type Format: Copy;
+    #[doc(hidden)]
     type SwapchainImage;
 
+    #[doc(hidden)]
     fn raise_format(x: i64) -> Self::Format;
+    #[doc(hidden)]
     fn lower_format(x: Self::Format) -> i64;
 
+    #[doc(hidden)]
     fn requirements(instance: &Instance, system: SystemId) -> Result<Self::Requirements>;
 
+    #[doc(hidden)]
     unsafe fn create_session(
         instance: Instance,
         system: SystemId,
         info: &Self::SessionCreateInfo,
     ) -> Result<Session<Self>>;
 
+    #[doc(hidden)]
     fn enumerate_swapchain_images(swapchain: &Swapchain<Self>) -> Result<Vec<Self::SwapchainImage>>;
 }
 
