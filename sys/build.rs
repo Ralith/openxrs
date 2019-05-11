c3c91a0960635ead5f1674537c6df31669156892
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-05-11 08:19:50 -0700

    Fix static loader build

--- sys/build.rs
+++ sys/build.rs
@@ -1,10 +1,10 @@
 #[cfg(feature = "static")]
 fn main() {
     let dst = cmake::Config::new("OpenXR-SDK")
-        .no_build_target(true)
         .define("BUILD_API_LAYERS", "OFF")
         .define("BUILD_TESTS", "OFF")
         .define("OpenGL_GL_PREFERENCE", "GLVND")
+        .define("DYNAMIC_LOADER", "OFF")
         .build();
 
     println!(
