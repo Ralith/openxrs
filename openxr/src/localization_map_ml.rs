1a66164e5edf456bab667f92b0df53873618957d
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2024-02-23 22:59:13 +0100

    LocalizationMapML with public fields instead of getters

--- openxr/src/localization_map_ml.rs
+++ openxr/src/localization_map_ml.rs
@@ -2,32 +2,18 @@ use crate::*;
 
 #[derive(Clone)]
 pub struct LocalizationMapML {
-    inner: sys::LocalizationMapML,
+    pub name: String,
+    pub map_uuid: UuidEXT,
+    pub map_type: LocalizationMapTypeML,
 }
 
 impl LocalizationMapML {
     #[inline]
     pub fn from_raw(inner: sys::LocalizationMapML) -> Self {
-        Self { inner }
-    }
-    #[inline]
-    pub fn as_raw(&self) -> sys::LocalizationMapML {
-        self.inner
-    }
-    #[inline]
-    pub fn name(&self) -> &str {
-        unsafe {
-            std::ffi::CStr::from_ptr(self.inner.name.as_ptr())
-                .to_str()
-                .unwrap()
+        Self {
+            name: unsafe { fixed_str(&inner.name).into() },
+            map_uuid: inner.map_uuid,
+            map_type: inner.map_type,
         }
-    }
-    #[inline]
-    pub fn map_uuid(&self) -> UuidEXT {
-        self.inner.map_uuid
-    }
-    #[inline]
-    pub fn map_type(&self) -> LocalizationMapTypeML {
-        self.inner.map_type
     }
 }
