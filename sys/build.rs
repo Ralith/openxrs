dcabda104a23b844a09d437758ed027f514c0ea0
Tim Balsfulland <tim@balsfulland.email>
2026-08-22 22:58:29 +0200

    add missing Android libraries when using `static` feature

--- sys/build.rs
+++ sys/build.rs
@@ -26,6 +26,46 @@ fn main() {
             // The OpenXR loader requires linking to `advapi32` on Windows:
             // https://github.com/KhronosGroup/OpenXR-SDK-Source/blob/58e026e7efa5a4f6a612c554560b53aa482f04d4/src/loader/CMakeLists.txt#L245
             println!("cargo:rustc-link-lib=advapi32");
+        } else if target_os == "android" {
+            // The OpenXR loader requires linking to the `log` and `android` libraries on Android:
+            // https://github.com/KhronosGroup/OpenXR-SDK-Source/blob/c610211f38f4e1e4ac811ced6135e144eedc7cf2/src/loader/CMakeLists.txt#L161-L163
+            // Note that the Rust `std` also currently links `log` on Android,
+            // but that might change in the future, so we link it as well.
+            println!("cargo:rustc-link-lib=log");
+            println!("cargo:rustc-link-lib=android");
+
+            // The OpenXR loader needs a C++ runtime library, but Android systems don't have it.
+            //
+            // A statically linked C++ runtime library is used by default.
+            // It's reasonable to assume that the user wants a fully static build when using the `static` feature.
+            //
+            // The pre-built loader distributed by Khronos is also built using the static C++ runtime library:
+            // https://github.com/KhronosGroup/OpenXR-SDK-Source/blob/c610211f38f4e1e4ac811ced6135e144eedc7cf2/maintainer-scripts/build-aar.sh#L50
+            //
+            // `c++_static` can cause issues when multiple C++ runtime libraries are included in one application:
+            // https://developer.android.com/ndk/guides/cpp-support
+            // In that case the user can set `OPENXRS_ANDROID_STL` to choose a different method of linking.
+            println!("cargo:rerun-if-env-changed=OPENXRS_ANDROID_STL");
+            let cxx_runtime_library = std::env::var("OPENXRS_ANDROID_STL");
+            match cxx_runtime_library.as_ref().map(String::as_str) {
+                Ok("static") | Err(std::env::VarError::NotPresent) => {
+                    println!("cargo:rustc-link-lib=c++_static");
+                    // I can't find good documentation on this,
+                    // but some symbols are defined in `c++abi` instead of `c++_static`.
+                    // https://libcxxabi.llvm.org/#:~:text=Why%20are%20the%20destructors%20for%20the%20standard%20exception%20classes%20defined%20in%20libc%2B%2Babi%3F
+                    println!("cargo:rustc-link-lib=c++abi");
+                }
+                Ok("shared") => {
+                    // requires `libc++_shared.so` to be included in the APK
+                    println!("cargo:rustc-link-lib=c++_shared");
+                }
+                Ok("none") => {
+                    // user will provide required C++ runtime library
+                }
+                var @ Err(std::env::VarError::NotUnicode(_)) | var @ Ok(_) => {
+                    panic!("invalid value for OPENXRS_ANDROID_STL env var: {var:?}");
+                }
+            }
         } else {
             println!("cargo:rustc-link-lib=stdc++");
         }
